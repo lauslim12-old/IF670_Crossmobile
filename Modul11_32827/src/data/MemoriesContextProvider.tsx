45559deb74975997b97b59d902a24a4e9b65d21e
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-11-27 20:42:37 +0700

    Forgot to add 'useEffect'

--- Modul11_32827/src/data/MemoriesContextProvider.tsx
+++ Modul11_32827/src/data/MemoriesContextProvider.tsx
@@ -1,8 +1,16 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
+import { getMemories } from '../firebase';
+
 import MemoriesContext, { Memory } from './memories-context';
 
 const MemoriesContextProvider = ({ children }: { children: ReactNode }) => {
   const [memories, setMemories] = useState<Memory[]>([]);
+
+  useEffect(() => {
+    getMemories()
+      .then((res) => setMemories(res))
+      .catch((err) => console.error(err));
+  }, []);
 
   const addMemory = (
     imageUrl: string,
