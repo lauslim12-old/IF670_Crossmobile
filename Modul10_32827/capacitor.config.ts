dd9905c29a1d7f07a109af9b9476c2f8b1235f48
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-12-03 22:44:43 +0700

    Fix naming

--- Modul10_32827/capacitor.config.ts
+++ Modul10_32827/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ac.id.umn.tigaduadelapanduatujuh.moduldelapan',
-  appName: 'Modul08_32827',
+  appId: 'ac.id.umn.crossmobile',
+  appName: 'Modul10_32827',
   webDir: 'build',
   bundledWebRuntime: false,
 };
